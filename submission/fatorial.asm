93f4c25d0b12331ff89aa6b74ecc6e7ab71b6467
Seu Nome <seuemail@example.com>
2025-02-22 01:17:58 +0000

    1

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,31 +1,31 @@
 @ /000 ; Endereço inicial do programa
 
 START LD N ; Carrega o valor de N no acumulador
-SC SUBR ; Chama a sub-rotina SUBR
-HM =0 ; Finaliza a execução do programa
+      SC SUBR ; Chama a sub-rotina SUBR
+END   HM =0 ; Finaliza a execução do programa
 
 ; Definição de variáveis
 @ /100 ; Endereço base para variáveis
-N K /0000 ; Variável N (entrada)
-RES K /0000 ; Variável RES (resultado)
+N     K /0000 ; Variável N (entrada)
+RES   K /0000 ; Variável RES (resultado)
 
 ; Sub-rotina para cálculo do fatorial
 @ /200 ; Início da sub-rotina
 
 SUBR K /0000 ; Inicializa SUBR
 JZ ZERO ; Se o valor for zero, pula para ZERO
-MM 1 ; Armazena 1 temporariamente em memória
-MM RES ; Inicializa RES com 1
+MM i ; 
+MM RES ; I
 
-LOOP LD 1 ; Carrega o valor de 1 no acumulador
+LOOP LD i ; 
 SB UM ; Subtrai UM do acumulador
-MM 1 ; Armazena o novo valor de volta em 1
+MM i ; 
 JZ ENDSUBR ; Se 1 for zero, finaliza a sub-rotina
 M RES ; Multiplica o acumulador pelo RES
 MM RES ; Armazena o novo valor de RES
 JP LOOP ; Volta para LOOP para continuar o cálculo
 
-ZERO LV -1 ; Define o valor de RES como 1 para 0! = 1
+ZERO LV =1 ; Define o valor de RES como 1 para 0! = 1
 MM RES ; Armazena o valor em RES
 
 ENDSUBR RS SUBR ; Retorna da sub-rotina
@@ -33,4 +33,4 @@ ENDSUBR RS SUBR ; Retorna da sub-rotina
 ; Definição de constantes
 @ /390
 UM K =1 ; Constante UM = 1
-1 K =0 ; Inicialização do registrador auxiliar
+i K =0 ; Inicialização do registrador auxiliar
