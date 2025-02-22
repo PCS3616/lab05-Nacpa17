8c8e04594380c969c835c8207689972d1847b89b
Nacpa17 <145813954+Nacpa17@users.noreply.github.com>
2025-02-21 22:57:11 -0300

    Update fatorial.asm

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,37 +1,30 @@
-; Calcula o fatorial de um número armazenado em 0x100 e armazena o resultado em 0x102
+        @000
+INICIO  LD N
+        SC CALC_FAT
+        HM =0
 
-    ORG 0x000       ; Define o início do código na posição 0x000
+        @100
+N       K /0000
+RESULT  K /0000
 
-START:
-    LD N            ; Carrega o valor de N no AC
-    SC SUBR         ; Chama a sub-rotina para calcular o fatorial
-    HM              ; Para a máquina
+        @200
+CALC_FAT K /0000
+        JZ CASO_ZERO
+        MM TEMP
+        MM RESULT
 
-    ORG 0x100       ; Define o início dos dados na posição 0x100
+CICLO   LD TEMP
+        SB UM
+        MM TEMP
+        JZ FINALIZA
+        ML RESULT
+        MM RESULT
+        JP CICLO
 
-N:      DW 5        ; Armazena o número para calcular o fatorial (por exemplo, 5)
-RES:    DW 0        ; Armazena o resultado do fatorial
+CASO_ZERO LV =1
+        MM RESULT
+FINALIZA RS CALC_FAT
 
-    ORG 0x200       ; Define o início da sub-rotina na posição 0x200
-
-SUBR:   K /0000     ; Início da sub-rotina
-        JZ ZERO     ; Se N for 0, pula para ZERO (fatorial de 0 é 1)
-        MM i        ; Armazena o valor de N em i
-        MM RES      ; Inicializa RES com N
-LOOP:   LD i        ; Carrega o valor de i no AC
-        SB UM       ; Subtrai 1 de i
-        MM i        ; Armazena o novo valor de i
-        JZ ENDSUBR  ; Se i for 0, pula para ENDSUBR
-        ML RES      ; Multiplica RES pelo valor de i
-        MM RES      ; Armazena o resultado em RES
-        JP LOOP     ; Repete o loop
-ZERO:   LV =1       ; Carrega 1 no AC (fatorial de 0 é 1)
-        MM RES      ; Armazena 1 em RES
-ENDSUBR RS SUBR     ; Retorna da sub-rotina
-
-    ORG 0x300       ; Define o início das constantes na posição 0x300
-
-UM:     DW 1        ; Constante 1
-i:      DW 0        ; Variável temporária para armazenar o valor atual de N
-
-    END             ; Fim do programa
+        @390
+UM      K =1
+TEMP    K =0
